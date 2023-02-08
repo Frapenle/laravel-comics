67d8b4e4ee53ec79e4a105cf3fbc7dc010059ad5
francescotrudu <francescotrudu84@gmail.com>
2023-02-08 17:34:04 +0100

    Add section hero

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('hero')
+    <div class="hero">
+        <img src="{{ Vite::asset('resources/assets/images/jumbotron.jpg')}}" alt="">
+    </div>
+    
+@endsection
