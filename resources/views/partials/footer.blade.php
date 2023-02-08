8654b3f490a6482261fd4d1fb27449598cbc13f0
francescotrudu <francescotrudu84@gmail.com>
2023-02-08 18:06:15 +0100

    Add Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,69 @@
+<footer>
+    <section class="footer-top">
+            <div class="container">
+                <nav>
+                    <!-- first column list -->
+                    <ul>
+                        <li><h3>DC Comics</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                        <!--  -->
+                        <li><h3>Shop</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Characters</a></li>
+                    </ul>
+
+                    <!-- second column list -->
+                    <ul>
+                        <li><h3>DC Comics</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <!-- third column list -->
+                    <ul>
+                        <li><h3>DC Comics</h3></li>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+        <section class="footer-bottom">
+            <div class="container">
+                <div class="footer-left">
+                    <button><a href="#">Sign up now!</a></button>
+                </div>
+                <div class="footer-right">
+                    <div class="social-network">
+                        <h3>Follow us</h3>
+                        <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-facebook.png')}}" alt="Facebook"></a>
+                        <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-twitter.png')}}" alt="Twitter"></a>
+                        <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-youtube.png')}}" alt="YouTube"></a>
+                        <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-pinterest.png')}}" alt="Pinterest"></a>
+                        <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-periscope.png')}}" alt="Periscope"></a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </footer>
+</footer>
