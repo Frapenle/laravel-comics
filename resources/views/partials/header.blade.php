2a0e0831eedb5092d24dfc150adfa5cf9c940e28
francescotrudu <francescotrudu84@gmail.com>
2023-02-08 17:33:54 +0100

    Add link to home

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,7 +4,7 @@
             <a href="{{ route('home') }}"><img src="{{ Vite::asset('resources/assets/images/dc-logo.png')}}" alt="DC logo"></a>
         </div>
         <ul>
-            <li><a href="#">link 1</a></li>
+            <li><a href="{{route('home')}}">Home</a></li>
             <li><a href="{{route('comics')}}">comics</a></li>
             <li><a href="#">link 3</a></li>
             <li><a href="#">link 4</a></li>
