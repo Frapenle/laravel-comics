b21a83f7ed7a61ad79c6bb082edc7cb5dca4553b
francescotrudu <francescotrudu84@gmail.com>
2023-02-08 16:14:31 +0100

    Add header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <nav class="container">
+        <div class="logo al">
+            <a href="{{ route('home') }}"><img src="{{ Vite::asset('resources/assets/images/dc-logo.png')}}" alt="DC logo"></a>
+        </div>
+        <ul>
+            <li><a href="#">link 1</a></li>
+            <li><a href="#">link 2</a></li>
+            <li><a href="#">link 3</a></li>
+            <li><a href="#">link 4</a></li>
+            <li><a href="#">link 5</a></li>
+        </ul>
+        <div></div>
+    </nav>
+</header>
