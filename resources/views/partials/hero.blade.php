06f8b3cf7e058b15a88b90f33e25c61547ea5099
francescotrudu <francescotrudu84@gmail.com>
2023-02-08 23:20:10 +0100

    Fix hero

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,8 +1,4 @@
-@extends('layouts.app')
 
-@section('hero')
     <div class="hero">
         <img src="{{ Vite::asset('resources/assets/images/jumbotron.jpg')}}" alt="">
     </div>
-    
-@endsection
