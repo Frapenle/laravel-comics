5ea6139e67ba85fca8d88d33b4e65a449941e396
francescotrudu <francescotrudu84@gmail.com>
2023-02-08 23:20:50 +0100

    Card

--- resources/views/partials/comics.blade.php
+++ resources/views/partials/comics.blade.php
@@ -1,20 +1,26 @@
 @extends('layouts.app')
 
 @section('main')
-<div class="row">
-    @foreach ($comics as $comic)
-        <div class="col">
-            <div class="card">
-                <div class="img">
-            <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
-                </div>
-                <div class="series">
-                    <h5>{{ $comic['title'] }}</h5>
+@include('partials/hero')
+<section id="cards">
+    <div class="container">
+        
+        <div class="row">
+            @foreach ($comics as $comic)
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+                <div class="card-group">
+                    <div class="card">
+                        <img src="{{ $comic['thumb']}}" class="card-img-top" alt="{{$comic['title']}}">
+                        <div class="card-body">
+                            <h6 class="card-title"> {{$comic['title']}} </h6>
+                        </div>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        
-    @endforeach
-</div>
+    </div>
+
+</section>
     
 @endsection
